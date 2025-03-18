9fb77cf70f798709f82fec52e78b3c4a299d13da
BaroiuVlad <baroiuvladut@gmail.com>
2025-03-18 21:57:10 +0200

    first commit

---
+++ StocareDateNiveluri/AdministrareFisier.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProiectPIU;
+
+namespace StocareDateNiveluri
+{
+    public class AdministrareFisier
+    {
+        private const int nr_max_jucatori = 50;
+        private string numeFisier;
+
+        public AdministrareFisier(string numeFisier)
+        {
+            this.numeFisier = numeFisier;
+            // deschiderea fisierului in modul OpenOrCreate
+            // se creeaza daca nu exista
+            Stream streamFisierText = File.Open(numeFisier, FileMode.OpenOrCreate);
+            streamFisierText.Close();
+        }
+
+        public void AddJucator(Jucator jucator)
+        {
+            using (StreamWriter streamWriterFisierText = new StreamWriter(numeFisier, true))
+            {
+                streamWriterFisierText.WriteLine(jucator.ConversieLaSir_PentruFisier());
+            }
+        }
+
+
+
+
+    }
+}
---
+++ StocareDateNiveluri/AdministrareJucatori_Memorie.cs
@@ -0,0 +1,53 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProiectPIU;
+
+namespace StocareDateNiveluri
+{
+    public class AdministrareJucatori_Memorie
+    {
+        private Jucator[] jucatori;
+        private int nrJucatori;
+
+        private const int nr_max_jucatori = 50;
+
+        public AdministrareJucatori_Memorie()
+        {
+            jucatori = new Jucator[nr_max_jucatori];
+            nrJucatori = 0;
+            
+        }
+
+        public void AdaugareJucator(Jucator jucator)
+        {
+            jucatori[nrJucatori] = jucator;
+            nrJucatori++;
+        }
+
+        public Jucator[] GetJucators(out int nrJucatori)
+        {
+            nrJucatori = this.nrJucatori;
+            return jucatori;
+        }
+
+        public static void IdentificareDupaNume(Jucator jucator)
+        {
+            Console.Write("Introduceti un nume:");
+            string nume1 = Console.ReadLine();
+
+            if (nume1 == jucator.nume)
+            {
+                Console.WriteLine($"Inaltimea Jucatorului {jucator.nume} este {jucator.inaltime}");
+            }
+            else
+            {
+                Console.WriteLine("Numele introdus nu corespunde");
+            }
+        }
+
+    }
+}
